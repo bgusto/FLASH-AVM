e64021c15fa2ab9cc42d58eb65664b1db5d1c39b
blg13 <blg13@my.fsu.edu>
2020-11-02 17:31:07 -0500

    Adding GrabIntegralQuantity routine that should be able to read any form of IQ file from FLASH.

--- src/GrabIntegralQuantity.m
+++ src/GrabIntegralQuantity.m
@@ -1,38 +1,53 @@
-function [iq t] = GrabIntegralQuantity(filenm, iqnm)
+function [iq t] = GrabIntegralQuantity(filenm, iqnm, indx)
 
 %-------------------------------------------------------------------------------%
 % Info:
 %
 % Inputs:
-%   filenm -  the integral quantities filename
-%   ignm -   the integral quantity name to grab
+%   filenm - the integral quantities filename
+%   iqnm   - the integral quantity name to grab
+%   indx   - the index, which can override auto search
 %
 % Outputs:
 %   iq - the integral quantity 
-%   t  - time
+%   t  - time (always output)
 %
 %-------------------------------------------------------------------------------%
 
-% first parse the header (at least three spaces for delimter)
-fid = fopen(filenm);
-header = 'header';
-nheaders = 1;
-while contains(header,'header')
-    header = strsplit(fgetl(fid), '   ');
-    nheaders = nheaders + 1;
+    % load the data
+    [iqraw delim nhead] = importdata(filenm);
+    
+    % switch if no header detected...
+    if nhead == 0
+
+        % check if indx present
+        if nargin < 3
+            error('If file header not present, user must provide index of desired variable.');
+        end
+
+        % get the desired quantitiy + time
+        t = iqraw(:,1);
+        iq = iqraw(:,indx);
+
+    else
+
+        % provided indx can override search
+        if nargin < 3
+
+            % determine desired index by matching string
+            for i = 1:length(iqraw.colheaders)
+              if contains(iqraw.colheaders{i},iqnm)
+                indx = i;
+              end
+            end
+
+        end
+
+        % get the desired quantitiy + time
+        t = iqraw.data(:,1);
+        iq = iqraw.data(:,indx);
+
+    end
+
+
 end
-fclose(fid);
-
-% determine desired index by matching string
-for i = 1:length(header)
-  if contains(header{i},iqnm)
-    indx = i;
-  end
-end
-
-% load the data
-iqraw = importdata(filenm,' ',nheaders);
-
-% get the desired quantitiy + time
-t = iqraw.data(:,1);
-iq = iqraw.data(:,indx);
